ecc6ff631e11afada7573f4e1bb173104576f0f7
Jamire44 <2003doyle@gmail.com>
2024-02-02 22:44:14 +0000

    First Grid attempt

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,3 +1,14 @@
 public class Battleship {
+    public static void main(String[] args) {
 
+        int[][] grid = new int[3][3];
+
+        for (int i = 0; i < grid.length;i++){
+            System.out.println("------");
+            for (int j = 0; j < grid[i].length;j++){
+                System.out.print(grid[i][j] + "|");
+            }
+            System.out.println();
+        }
+    }
 }//class
