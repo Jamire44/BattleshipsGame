29e256aa90f9f76066106e49bca989ac2dc68b48
Jamire44 <2003doyle@gmail.com>
2024-02-03 20:10:54 +0000

    Put Logic in its own class w/ methods

--- src/Battleship.java
+++ src/Battleship.java
@@ -3,45 +3,17 @@ import java.util.Scanner;
 public class Battleship {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int col;
-        int row;
-        char[][] grid = new char[3][3];
+        final int numberOfShips = 3;
 
-        for (int i = 0; i < grid.length;i++){
-            System.out.println("------");
-            for (int j = 0; j < grid[i].length;j++){
-                System.out.print(grid[i][j] + "|");
-            }
-            System.out.println();
+        Logic logic = new Logic();
+
+        for (int i = 0; i < numberOfShips; i++){
+            logic.Grid();
+            logic.whereShipsWantToBePlaces();
+            logic.updatingShips(); 
         }
-
-        System.out.println("Where do you want to place your Ship: (Use index[0][1] for example): ");
-        System.out.println("What column: ");
-        col = scanner.nextInt();
-        System.out.println("What row: ");
-        row = scanner.nextInt();
-
-        for (int i = 0; i < grid.length;i++){
-            for (int j = 0; j < grid[i].length;j++){
-                if (grid[col][row] == grid[i][j]){
-                    grid[i][j] = 'x';
-                    for (int k = grid[col][row-1]; k <= 0; k--){
-                        grid[i][j] = 'O';
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < grid.length;i++){
-            System.out.println("------");
-            for (int j = 0; j < grid[i].length;j++){
-                System.out.print(grid[i][j] + "|");
-            }
-            System.out.println();
-        }
-
-
 
     }
 }//class
+
+
--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,53 @@
+import java.util.Scanner;
+
 public class Logic {
+
+    public Logic() {
+
+    }
+
+    char[][] grid = new char[4][4];
+    int col;
+    int row;
+
+    public char[][] Grid(){
+
+        for (int i = 0; i < grid.length;i++){
+            System.out.println("--------");
+            for (int j = 0; j < grid[i].length;j++){
+                System.out.print(grid[i][j] + "|");
+            }
+            System.out.println();
+        }
+        return grid;
+    }
+
+    public void whereShipsWantToBePlaces(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Where do you want to place your Ship: (Use index[0][1] for example): ");
+        System.out.println("What column: ");
+        col = scanner.nextInt();
+        System.out.println("What row: ");
+        row = scanner.nextInt();
+
+    }
+
+    public void updatingShips(){
+
+        for (int i = 0; i < grid.length - 1;i++){
+            for (int j = 0; j < grid[i].length;j++){
+                if (grid[col][row] == grid[i][j]){
+                    grid[i][j] = 'x';
+                    for (int k = grid[col][row-1]; k <= 0; k--){
+                        grid[i][j] = 'O';
+                    }
+                }
+            }
+        }
+    }
+
+
 
 
 
