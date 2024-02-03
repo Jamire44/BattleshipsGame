378c4379884d3e485759dd695df90ed2210f79d4
Jamire44 <2003doyle@gmail.com>
2024-02-03 15:03:49 +0000

    User to place a ship logic added

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Battleship {
     public static void main(String[] args) {
 
-        int[][] grid = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+        int col;
+        int row;
+        char[][] grid = new char[3][3];
 
         for (int i = 0; i < grid.length;i++){
             System.out.println("------");
@@ -10,5 +15,33 @@ public class Battleship {
             }
             System.out.println();
         }
+
+        System.out.println("Where do you want to place your Ship: (Use index[0][1] for example): ");
+        System.out.println("What column: ");
+        col = scanner.nextInt();
+        System.out.println("What row: ");
+        row = scanner.nextInt();
+
+        for (int i = 0; i < grid.length;i++){
+            for (int j = 0; j < grid[i].length;j++){
+                if (grid[col][row] == grid[i][j]){
+                    grid[i][j] = 'x';
+                    for (int k = grid[col][row-1]; k <= 0; k--){
+                        grid[i][j] = 'O';
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < grid.length;i++){
+            System.out.println("------");
+            for (int j = 0; j < grid[i].length;j++){
+                System.out.print(grid[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+
+
     }
 }//class
---
+++ src/Ships.java
@@ -0,0 +1,15 @@
+public class Ships {
+
+    int lengthOfShip;
+    public Ships(int lengthOfShip){
+        this.lengthOfShip = lengthOfShip;
+    }
+
+    public int getLengthOfShip() {
+        return lengthOfShip;
+    }
+
+    public void setLengthOfShip(int lengthOfShip) {
+        this.lengthOfShip = lengthOfShip;
+    }
+}//class
